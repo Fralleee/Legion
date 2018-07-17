d8a1772ce8edf8c71cf42aa1ea2ddeaf2c3dc4be
Roland Chelwing <jolleee@hotmail.com>
2018-07-17 15:58:50 +0200

    xd

--- Legion/Assets/Building System/Scripts/Placeable.cs
+++ Legion/Assets/Building System/Scripts/Placeable.cs
@@ -9,12 +9,11 @@ public class Placeable : ScriptableObject
   public float wood;
   [SerializeField] GameObject prefab;  
   GameObject instance;
-
+  
   public void Initiate(Transform parent)
   {
     instance = Instantiate(prefab, new Vector3(parent.position.x, 0, parent.position.z) + parent.transform.forward * 2, Quaternion.Euler(0,0,0));
     instance.GetComponent<SnapToGrid>().parent = parent;
-    Debug.Log(instance);
   }
 
   public void Cancel()
