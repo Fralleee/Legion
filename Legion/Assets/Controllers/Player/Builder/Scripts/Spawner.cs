36934368e3e6960d1747ae632106d50bc5e1dcc2
Roland Chelwing <jolleee@hotmail.com>
2018-07-31 15:31:48 +0200

    Spawns fixed

--- Legion/Assets/Controllers/Player/Builder/Scripts/Spawner.cs
+++ Legion/Assets/Controllers/Player/Builder/Scripts/Spawner.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour {
-
+public class Spawner : MonoBehaviour
+{
+  [SerializeField] GameObject buildingUnitGraphics;
   [SerializeField] GameObject unitPrefab;
+
   public void SpawnUnit()
   {
+    buildingUnitGraphics.SetActive(false);
     Instantiate(unitPrefab, transform.position, transform.rotation);
   }
 
+  public void Reset()
+  {
+    buildingUnitGraphics.SetActive(true);
+  }
 }
