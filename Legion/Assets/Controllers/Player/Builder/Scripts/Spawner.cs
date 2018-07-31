8fc4e8e9602a370177e33337f6a1a0e6f14b191f
Roland Chelwing <jolleee@hotmail.com>
2018-07-31 18:07:45 +0200

    xdss

--- Legion/Assets/Controllers/Player/Builder/Scripts/Spawner.cs
+++ Legion/Assets/Controllers/Player/Builder/Scripts/Spawner.cs
@@ -7,15 +7,37 @@ public class Spawner : MonoBehaviour
   [SerializeField] GameObject buildingUnitGraphics;
   [SerializeField] GameObject unitPrefab;
 
-  public void SpawnUnit()
+  new Renderer graphicsRenderer;
+  Material material;
+  float time;
+  bool regenerate;
+
+  void Awake()
   {
-    buildingUnitGraphics.SetActive(false);
-    GameObject instance = Instantiate(unitPrefab, transform.position, transform.rotation);
-    instance.name = unitPrefab.name;
+    graphicsRenderer = buildingUnitGraphics.GetComponentInChildren<Renderer>();
+    graphicsRenderer.material.shader = Shader.Find("graphs/Hologram");
   }
 
-  public void Reset()
+  public void SpawnUnit()
   {
-    buildingUnitGraphics.SetActive(true);
+    graphicsRenderer.material.SetFloat("Vector1_6BA813AB", 0f);
+    Instantiate(unitPrefab, transform.position, transform.rotation);
+  }
+
+  public void StartRegeneration()
+  {
+    regenerate = true;
+    time = -2f;
+  }
+
+  void Update()
+  {
+    if(regenerate)
+    {
+      Debug.Log(time);
+      time += Time.deltaTime * 0.1f;
+      graphicsRenderer.material.SetFloat("Vector1_6BA813AB", Mathf.Lerp(0, 0.25f, time));
+      if (time >= 1) regenerate = false;
+    }
   }
 }
