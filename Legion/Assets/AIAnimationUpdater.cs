fc09ddd3c6b0ffdce2bd4d2244957abed4892540
Roland Chelwing <jolleee@hotmail.com>
2018-08-05 14:44:10 +0200

    Added some animations for AI

---
+++ Legion/Assets/AIAnimationUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIAnimationUpdater : MonoBehaviour
+{
+
+  NavMeshAgent navMeshAgent;
+  Animator animator;
+
+  void Start()
+  {
+    navMeshAgent = GetComponent<NavMeshAgent>();
+    animator = GetComponentInChildren<Animator>();
+  }
+
+  void Update()
+  {
+    Vector3 movement = transform.TransformDirection(navMeshAgent.velocity);
+    animator.SetFloat("Vertical", Mathf.Abs(movement.x), .1f, Time.deltaTime);
+    animator.SetFloat("Horizontal", Mathf.Abs(movement.z), .1f, Time.deltaTime);
+  }
+}
