b2e7527be9cd85e78e93cd6401a22a83ea028549
Roland Chelwing <jolleee@hotmail.com>
2018-08-14 14:33:52 +0200

    Cleanup

--- Legion/Assets/Libraries/Fralle.Systems/AbilitySystem/Base/AbilityEffect.cs
+++ Legion/Assets/Libraries/Fralle.Systems/AbilitySystem/Base/AbilityEffect.cs
@@ -1,8 +1,19 @@
-﻿using System.Collections;
+﻿using Fralle;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class AbilityEffect : ScriptableObject
 {
-  public virtual void Affect(Ability ability, GameObject target) { }
+  public ParticleSystem onHitEffect;
+  public virtual void Affect(Ability ability, GameObject target)
+  {
+    if (onHitEffect)
+    {
+      ParticleSystem onHit = Instantiate(onHitEffect, target.transform.position.WithY(target.transform.localScale.y), Quaternion.identity);
+      onHit.transform.localScale = target.transform.localScale;
+      Destroy(onHit.gameObject, onHit.main.startLifetime.constant);
+    }
+    else Debug.LogWarning("No OnHitEffect for Effect: " + name);
+  }
 }
--- Legion/Assets/Libraries/Fralle.Systems/AbilitySystem/Derived/Effects/DamageEffect.cs
+++ Legion/Assets/Libraries/Fralle.Systems/AbilitySystem/Derived/Effects/DamageEffect.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Abilities/Effects/Damage")]
 public class DamageEffect : AbilityEffect
 {
-  public ParticleSystem onHitEffect;
   public override void Affect(Ability ability, GameObject target)
   {
     base.Affect(ability, target);
@@ -15,12 +14,5 @@ public class DamageEffect : AbilityEffect
     float amount = rnd.Next(ability.MinAmount, ability.MaxAmount);
     damageController.TakeDamage(amount, ability.Caster, ability.AbilityName);
 
-    if (onHitEffect)
-    {
-      ParticleSystem onHit = Instantiate(onHitEffect, target.transform.position.WithY(target.transform.localScale.y), Quaternion.identity);
-      onHit.transform.localScale = target.transform.localScale;
-      Destroy(onHit.gameObject, onHit.main.startLifetime.constant);
-    }
-    else Debug.LogWarning("No OnHitEffect for Effect: " + name);
   }
 }
