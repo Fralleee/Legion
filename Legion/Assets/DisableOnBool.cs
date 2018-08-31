d849a9404513a2afdbcbf09fab80e599ccda0b9b
Roland Chelwing <jolleee@hotmail.com>
2018-08-31 15:49:44 +0200

    xds

--- Legion/Assets/DisableOnBool.cs
+++ Legion/Assets/DisableOnBool.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisableOnBool : MonoBehaviour {
-
+public class DisableOnBool : MonoBehaviour
+{
   [SerializeField] BoolVariable value;
   [SerializeField] Behaviour[] behaviours;
-
-	void Update ()
+  void Awake() { value.OnChange += OnChange; }
+  void OnChange(bool newValue)
   {
-    foreach (Behaviour behaviour in behaviours)
-    {
-      behaviour.enabled = !value.currentValue;
-    }
-	}
+    foreach (Behaviour behaviour in behaviours) behaviour.enabled = newValue;
+  }
 }
