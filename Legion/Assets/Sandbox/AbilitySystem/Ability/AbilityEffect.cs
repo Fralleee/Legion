4cf0eff272bd15203cabec34b2a290e172485c0d
Roland Chelwing <jolleee@hotmail.com>
2018-09-10 21:02:46 +0200

    Gjort skit

--- Legion/Assets/Sandbox/AbilitySystem/Ability/AbilityEffect.cs
+++ Legion/Assets/Sandbox/AbilitySystem/Ability/AbilityEffect.cs
@@ -11,8 +11,8 @@ public abstract class AbilityEffect : ScriptableObject
     if (onHitEffect)
     {
       ParticleSystem onHit = Instantiate(onHitEffect, target.transform.position.With(y: target.transform.localScale.y), Quaternion.identity);
-      onHit.transform.localScale = target.transform.localScale;
-      Destroy(onHit.gameObject, onHit.main.startLifetime.constant);
+      onHit.transform.localScale = target.transform.localScale;      
+      Destroy(onHit.gameObject, onHit.main.duration);
     }
     else Debug.LogWarning("No OnHitEffect for Effect: " + name);
   }
